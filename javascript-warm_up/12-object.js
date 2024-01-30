725da2df5609ee2c6770bb57d9207bc7043b5587
sfanxAK <soufianeakhiat@gmail.com>
2024-01-30 18:37:27 +0100

    add

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+/* Update this script to replace the value 12 with 89:
+
+You are not allowed to use var */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+let  newValue = myObject.value + 77;
+console.log(myObject);
